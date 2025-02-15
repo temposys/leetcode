2b588da07dc25c9e13aad0c93bc9cc802d5358d0
Yana Tempos <ys.tempos@gmail.com>
2025-02-15 15:08:44 -0800

    Create file

---
+++ 700-SearchinaBinarySearchTree/700-SearchinaBinarySearchTree.php
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $val
+     * @return TreeNode
+     */
+    function searchBST($root, $val) {
+        if ($root === null) {
+            return null;
+        }
+        if ($root->val === $val) {
+            return $root;
+        }
+        return $this->searchBST($val > $root->val ? $root->right : $root->left, $val);
+    }
+}
