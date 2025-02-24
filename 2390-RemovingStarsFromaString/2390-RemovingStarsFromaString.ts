922b7bf926b3c6925dca86b8eadd3c005de6be44
Yana Tempos <ys.tempos@gmail.com>
2025-02-23 19:23:37 -0800

    File updated at 23/02/2025, 19:23:37

--- 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.ts
+++ 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.ts
@@ -1,11 +1,11 @@
+// Last updated: 23/02/2025, 19:23:37
 function removeStars(s: string): string {
-    let i: number = 0;
     let arr: string[] = [];
-    for (let letter of s) {
-        if (letter === '*') {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '*') {
             arr.pop();
         } else {
-            arr.push(letter);
+            arr.push(s[i]);
         }
     }
 
