8e971e359054ad2adb0f492e9efb827f4097ba73
Yana Tempos <ys.tempos@gmail.com>
2025-02-25 19:49:15 -0800

    Create file

---
+++ 50-Pow(x,n)/50-Pow(x,n).php
@@ -0,0 +1,38 @@
+class Solution {
+
+    /**
+     * @param Float $x
+     * @param Integer $n
+     * @return Float
+     */
+    function myPow($x, $n) {
+        if ($x === 0) return 0;
+        if ($n === 0) return 1;
+
+        if (abs($x) < PHP_FLOAT_MIN && $n < 0) {
+            return INF; // Avoid division by zero
+        }
+
+        $rasing = 1;
+        $deg = abs($n);
+
+        while ($deg != 0) {
+            if ($deg % 2 === 0) {
+                $x *= $x;
+                $deg /= 2;
+            } else {
+                $rasing *= $x;
+                $deg--;
+            }
+        }
+        // for ($i = 1; $i <= abs($n); $i++) {
+        //     $rasing *= $x;
+
+        //     if (abs($rasing) < PHP_FLOAT_MIN && $n < 0) {
+        //         return 0;
+        //     } 
+        // }
+
+        return $n > 0 ? $rasing : 1/$rasing;
+    }
+}
