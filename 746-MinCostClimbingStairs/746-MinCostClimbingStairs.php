47f374b6cd7c553881d97c9ffa109cccbaac2b6d
Yana Tempos <ys.tempos@gmail.com>
2025-03-01 11:53:42 -0800

    File updated at 01/03/2025, 11:53:42

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.php
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.php
@@ -5,11 +5,22 @@ class Solution {
      * @return Integer
      */
     function minCostClimbingStairs($cost) { // [10, 15, 20]
-        $cost[] = 0; // [10, 15, 20, 0]
-        for ($i = count($cost) - 3; $i >= 0; $i--) { // 1 0
-            $cost[$i] += min($cost[$i+1], $cost[$i+2]); // [10, 15+0, 20, 0] [10+15, 15, 20, 0]
+        // $cost[] = 0; // [10, 15, 20, 0]
+        // for ($i = count($cost) - 3; $i >= 0; $i--) { // 1 0
+        //     $cost[$i] += min($cost[$i+1], $cost[$i+2]); // [10, 15+0, 20, 0] [10+15, 15, 20, 0]
+        // }
+
+        // return min($cost[0], $cost[1]); // min(25, 15) = 15
+
+        $first = $cost[0]; // 10
+        $second = $cost[1]; // 15
+
+        for ($i = 2; $i < count($cost); $i++) { // 2
+            $current = min($first, $second) + $cost[$i]; // 10 + 20
+            $first = $second; // 15
+            $second = $current; // 30
         }
 
-        return min($cost[0], $cost[1]); // min(25, 15) = 15
+        return min($first, $second); // min (15, 30) = 15
     }
 }
