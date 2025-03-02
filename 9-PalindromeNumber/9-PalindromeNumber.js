e6c9a819e57963f57e61fe9957463f0010483e25
Yana Tempos <ys.tempos@gmail.com>
2025-03-02 14:10:53 -0800

    File updated at 02/03/2025, 14:10:52

--- 9-PalindromeNumber/9-PalindromeNumber.js
+++ 9-PalindromeNumber/9-PalindromeNumber.js
@@ -10,23 +10,34 @@ var isPalindrome = function(x) {
         return true;
     }
 
+    let reverse = 0;
     let rest = x;
-    let arr = [];
+
     while (rest > 0) {
-        arr.push(rest % 10);
+        reverse = reverse * 10 + (rest % 10);
         rest = Math.floor(rest/10);
     }
 
-    let start = 0;
-    let end = arr.length - 1;
+    return reverse === x;
 
-    while (start <= end) {
-        if (arr[start] !== arr[end]) {
-            return false;
-        }
-        start++;
-        end--;
-    }
 
-    return true;
+    // let rest = x;
+    // let arr = [];
+    // while (rest > 0) {
+    //     arr.push(rest % 10);
+    //     rest = Math.floor(rest/10);
+    // }
+
+    // let start = 0;
+    // let end = arr.length - 1;
+
+    // while (start <= end) {
+    //     if (arr[start] !== arr[end]) {
+    //         return false;
+    //     }
+    //     start++;
+    //     end--;
+    // }
+
+    // return true;
 };
