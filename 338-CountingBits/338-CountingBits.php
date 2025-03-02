0b8bdd68aec8d387a2c42da199e01c63119670c8
Yana Tempos <ys.tempos@gmail.com>
2025-03-01 17:50:56 -0800

    File updated at 01/03/2025, 17:50:55

--- 338-CountingBits/338-CountingBits.php
+++ 338-CountingBits/338-CountingBits.php
@@ -5,11 +5,19 @@ class Solution {
      * @return Integer[]
      */
     function countBits($n) {
-        $res = [];
-        for ($i = 0; $i <= $n; $i++) {
-            $res[] = array_sum(str_split((string) decbin($i), 1));
+        $res = array_fill(0, $n+1, 0);
+
+        for ($i = 1; $i <= $n; $i++) {
+            $res[$i] = $res[$i >> 1] + ($i & 1);
         }
 
         return $res;
+
+        // $res = [];
+        // for ($i = 0; $i <= $n; $i++) {
+        //     $res[] = array_sum(str_split((string) decbin($i), 1));
+        // }
+
+        // return $res;
     }
 }
