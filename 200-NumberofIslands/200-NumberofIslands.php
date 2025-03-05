635f8eb92fe0e23aaf25637a442cf511acdc7994
Yana Tempos <ys.tempos@gmail.com>
2025-03-04 16:59:55 -0800

    Create file

---
+++ 200-NumberofIslands/200-NumberofIslands.php
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * @param String[][] $grid
+     * @return Integer
+     */
+    function numIslands($grid) {
+        $islands = 0;
+
+        for ($i = 0; $i < count($grid); $i++) {
+            for ($j = 0; $j < count($grid[0]); $j++) {
+                if ($grid[$i][$j] == 1) {
+                    $islands++;
+                    $this->dfs($grid, $i, $j);
+                }
+            }
+        }
+
+        return $islands;
+    }
+
+    function dfs(&$grid, $i, $j): void {
+        if (isset($grid[$i][$j]) && $grid[$i][$j] == 1) {
+            $grid[$i][$j] = 0;
+            $this->dfs($grid, $i+1, $j);
+            $this->dfs($grid, $i-1, $j);
+            $this->dfs($grid, $i, $j+1);
+            $this->dfs($grid, $i, $j-1);
+        }
+    }
+}
