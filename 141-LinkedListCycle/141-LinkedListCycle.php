dd8923b2fd9c6c94b20c4994a3c2a70e00c2c6af
Yana Tempos <ys.tempos@gmail.com>
2025-03-05 09:19:00 -0800

    File updated at 05/03/2025, 09:19:00

--- 141-LinkedListCycle/141-LinkedListCycle.php
+++ 141-LinkedListCycle/141-LinkedListCycle.php
@@ -14,18 +14,30 @@ class Solution {
      */
     function hasCycle($head) {
         if (!$head) return false;
-        $visited = [
-            spl_object_id($head) => true,
-        ];
 
-        $current = $head;
-        while ($current = $head->next) {
-            if (isset($visited[spl_object_id($current)])) {
+        $slow = $head;
+        $fast= $head;
+        while($fast != null && $fast->next != null){
+            $slow = $slow->next;
+            $fast = $fast->next->next;
+            // var_dump($slow->val, $fast->val);
+            if($fast===$slow){
                 return true;
             }
-            $visited[spl_object_id($current)] = true;
-            $head = $current;
         }
+
+        // $visited = [
+        //     spl_object_id($head) => true,
+        // ];
+
+        // $current = $head;
+        // while ($current = $head->next) {
+        //     if (isset($visited[spl_object_id($current)])) {
+        //         return true;
+        //     }
+        //     $visited[spl_object_id($current)] = true;
+        //     $head = $current;
+        // }
 
         return false;
     }
