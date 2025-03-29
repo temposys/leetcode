db221bc3f1813ef6a4d777a13c93170294db21b0
Yana Tempos <ys.tempos@gmail.com>
2025-03-28 20:28:15 -0700

    Create file

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.php
@@ -0,0 +1,38 @@
+// Last updated: 28/03/2025, 20:28:15
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function longestPalindrome($s) {
+        $length = strlen($s);
+        if ($length === 0) {
+            return "";
+        }
+        if (strrev($s) === $s) {
+            return $s;
+        }
+
+        $palindrome = "";
+
+        for ($i = 0; $i < $length; $i++) {
+            $longest = "";
+            $temp = "";
+
+            for ($j = $i; $j < $length; $j++) {
+                $temp .=$s[$j];
+
+                if ($temp === strrev($temp)) {
+                    $longest = $temp;
+                }
+            }
+
+            if (strlen($longest) > strlen($palindrome)) {
+                $palindrome = $longest;
+            }
+        }
+
+        return $palindrome;
+    }
+}
