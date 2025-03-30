2c1ae7f0588168072d8f3ad122d4b82eca61443a
Yana Tempos <ys.tempos@gmail.com>
2025-03-30 13:19:10 -0700

    File updated at 30/03/2025, 13:19:10

--- 20-ValidParentheses/20-ValidParentheses.php
+++ 20-ValidParentheses/20-ValidParentheses.php
@@ -1,4 +1,4 @@
-// Last updated: 30/03/2025, 13:01:19
+// Last updated: 30/03/2025, 13:19:10
 class Solution {
     private array $bracketMap = ['(' => ')', '{' => '}', '[' => ']'];
 
@@ -7,22 +7,17 @@ class Solution {
      * @return Boolean
      */
     function isValid($s) {
+        $length = strlen($s);
+        if ($length % 2 === 1) {
+            return false;
+        }
+
         $stack = [];
-        for ($i = 0; $i < strlen($s); $i++) {
-            if (in_array($s[$i], $this->bracketMap)) {
-                $lastElement = end($stack);
-                // closed bracket
-                if ($lastElement === null) {
-                    return false;
-                }
-                if ($s[$i] === $this->bracketMap[$lastElement]) {
-                    // pair found
-                    array_pop($stack); // remove last element
-                } else {
-                    array_push($stack, $s[$i]);
-                }
-            } else {
-                array_push($stack, $s[$i]);
+        for ($i = 0; $i < $length; $i++) {
+            if (isset($this->bracketMap[$s[$i]])) {
+                array_push($stack, $this->bracketMap[$s[$i]]);
+            } elseif ($s[$i] !== array_pop($stack)) {
+                return false;
             }
         }
 
