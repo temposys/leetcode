091d9452f40dcfb6b6b351e65ffbe123ef9bca66
Yana Tempos <ys.tempos@gmail.com>
2025-03-30 17:30:07 -0700

    File updated at 30/03/2025, 17:30:07

--- 735-AsteroidCollision/735-AsteroidCollision.php
+++ 735-AsteroidCollision/735-AsteroidCollision.php
@@ -1,4 +1,4 @@
-// Last updated: 30/03/2025, 17:00:34
+// Last updated: 30/03/2025, 17:30:07
 class Solution {
 
     /**
@@ -8,32 +8,26 @@ class Solution {
     function asteroidCollision($asteroids) {
         $stack = [$asteroids[0]];
         for ($i = 1; $i < count($asteroids); $i++) {
-            $collision = $this->collision(array_pop($stack), $asteroids[$i]);
-            while (count($collision) === 1 && count($stack) > 0) {
-                $collision = $this->collision(array_pop($stack), $collision[0]);
+            $collision = false;
+            while (!empty($stack) && end($stack) > 0 && $asteroids[$i] < 0) {
+                $collision = true;
+                if (end($stack) === abs($asteroids[$i])) {
+                    array_pop($stack);
+                    break;
+                } elseif (end($stack) < abs($asteroids[$i])) {
+                    $collision = false;
+                    array_pop($stack);
+                    continue;
+                } else {
+                    break;
+                }
             }
-            $stack = array_merge($stack, $collision);
+            if (!$collision) {
+                $stack[] = $asteroids[$i];
+            }
 
         }
 
         return $stack;
-    }
-
-    function collision($first, $second) {
-        if ($first === null) {
-            return [$second];
-        }
-
-        if ($first > 0 && $second < 0) {
-            if (abs($first) === abs($second)) {
-                return [];
-            } elseif (abs($first) < abs($second)) {
-                return [$second];
-            } else {
-                return [$first];
-            }
-        } 
-
-        return [$first, $second];
     }
 }
