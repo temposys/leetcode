8a19f6222aef67bb8b6cb4919e90d3dfa8e4ef44
Yana Tempos <ys.tempos@gmail.com>
2025-04-02 15:13:22 -0700

    File updated at 02/04/2025, 15:13:21

--- 875-KokoEatingBananas/875-KokoEatingBananas.php
+++ 875-KokoEatingBananas/875-KokoEatingBananas.php
@@ -1,4 +1,4 @@
-// Last updated: 02/04/2025, 15:11:46
+// Last updated: 02/04/2025, 15:13:21
 class Solution {
 
     /**
@@ -6,35 +6,34 @@ class Solution {
      * @param Integer $h
      * @return Integer
      */
-
-    function getHours($piles, $k) {
-        $hours = 0;
-
-        foreach ($piles as $pile) {
-            $hours += ceil($pile / $k);
-        }
-        return $hours;
-    }
-
     function minEatingSpeed($piles, $h) {
-        
-        $maxPiles = max($piles);
-
         $left = 1;
-        $right = $maxPiles;
+        $right = max($piles);
+        
+        while ($left < $right) {
+            $middle = intval(($right + $left) / 2);
 
-        while ($left <= $right) {
-            $mid = $left + floor(($right - $left) / 2);
-            $hoursForK = $this->getHours($piles, $mid);
-
-            if ($hoursForK <= $h) {
-                $right = $mid - 1;
-            }
-            else {
-                $left = $mid + 1;
+            if ($this->canEat($piles, $h, $middle)) {
+                $right = $middle;
+            } else {
+                $left = $middle + 1;
             }
         }
 
         return $left;
+    }
+
+    function canEat($piles, $h, $middle)
+    {
+        $hours = 0;
+
+        foreach($piles as $pile) {
+            $hours += ceil($pile/$middle);
+            if ($hours > $h) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
