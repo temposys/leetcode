2e667a0de00e606fbfd2ea4bde5b0d97f8c8d9ed
Yana Tempos <ys.tempos@gmail.com>
2025-04-10 11:04:06 -0700

    File updated at 10/04/2025, 11:04:06

--- 63-UniquePathsII/63-UniquePathsII.js
+++ 63-UniquePathsII/63-UniquePathsII.js
@@ -1,34 +1,33 @@
-// Last updated: 09/04/2025, 19:22:12
+// Last updated: 10/04/2025, 11:04:06
 /**
- * @param {number[][]} obstacleGrid
- * @return {number}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var uniquePathsWithObstacles = function(obstacleGrid) {
-    if (obstacleGrid[0][0] === 1) return 0;
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode();
+    let cur = dummy;
 
-    const m = obstacleGrid.length;
-    const n = obstacleGrid[0].length;
-
-    let grid = Array.from({ length: m }, () => Array(n).fill(0));
-
-    // first row
-    for (let j = 0; j < n; j++) {
-        if (obstacleGrid[0][j] === 1) break;
-        grid[0][j] = 1;
-    }
-
-    // first col
-    for (let i = 0; i < m; i++) {
-        if (obstacleGrid[i][0] === 1) break;
-        grid[i][0] = 1;
-    }
-
-
-    for (let i = 1; i < m; i++) {
-        for ( let j = 1; j < n; j++) {
-            grid[i][j] = (obstacleGrid[i][j] === 1) ? 0 : grid[i-1][j] + grid[i][j-1];
+    while (list1 && list2) {
+        if (list1.val > list2.val) {
+            cur.next = list2;
+            list2 = list2.next;
+        } else {
+            cur.next = list1;
+            list1 = list1.next;
         }
+        cur = cur.next;
     }
 
-    return grid[m-1][n-1];
+    cur.next = list1 || list2;
+
+    return dummy.next;    
 };
+
