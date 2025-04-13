a9dae8d282aa3d8dfb799538670ec9093b38fd16
Yana Tempos <ys.tempos@gmail.com>
2025-04-13 10:15:17 -0700

    File updated at 13/04/2025, 10:15:17

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.php
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.php
@@ -1,4 +1,4 @@
-// Last updated: 13/04/2025, 10:06:37
+// Last updated: 13/04/2025, 10:15:17
 class Solution {
 
     /**
@@ -7,15 +7,33 @@ class Solution {
      * @return Integer
      */
     function findKthLargest($nums, $k) {
-        $minHeap = new SplMinHeap();
+        $max = max($nums);
+
+        $count = array_fill(0, 20001, 0);
 
         foreach ($nums as $num) {
-            $minHeap->insert($num);
-            if ($minHeap->count() > $k) {
-                $minHeap->extract();
+            $count[10000+$num]++;
+        }
+
+        for ($i = count($count) - 1; $i >= 0; $i--) {
+            if ($count[$i] > 0) {
+                $k -= $count[$i];
+                if ($k <= 0) return $i - 10000;
             }
         }
 
-        return $minHeap->top();
+        return -1;
+
+
+        // $minHeap = new SplMinHeap();
+
+        // foreach ($nums as $num) {
+        //     $minHeap->insert($num);
+        //     if ($minHeap->count() > $k) {
+        //         $minHeap->extract();
+        //     }
+        // }
+
+        // return $minHeap->top();
     }
 }
