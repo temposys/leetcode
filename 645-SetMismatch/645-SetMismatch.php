247c2f605f8f96f81bd03cb919595e58d8a6a130
Yana Tempos <ys.tempos@gmail.com>
2025-04-17 17:11:12 -0700

    Create file

---
+++ 645-SetMismatch/645-SetMismatch.php
@@ -0,0 +1,26 @@
+// Last updated: 17/04/2025, 17:11:11
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function findErrorNums($nums) {
+        $count = count($nums);
+        $set = [];
+        $duplicated = -1;
+        $actualSum = 0;
+        $expectedSum = ($count*($count + 1)) / 2;
+ 
+        foreach ($nums as $key => $num) {
+            if (isset($set[$num])) {
+                $duplicated = $num;
+            } else {
+                $set[$num] = true;
+            }
+            $actualSum += $num;
+        }
+
+        return [$duplicated, $expectedSum - ($actualSum - $duplicated)];
+    }
+}
