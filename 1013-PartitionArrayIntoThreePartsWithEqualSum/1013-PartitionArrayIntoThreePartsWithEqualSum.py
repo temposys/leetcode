dadcbd9a64ffcd11387f2edd7e84cd2e52ba5e32
Yana Tempos <ys.tempos@gmail.com>
2025-04-18 14:48:29 -0700

    File updated at 18/04/2025, 14:48:29

--- 1013-PartitionArrayIntoThreePartsWithEqualSum/1013-PartitionArrayIntoThreePartsWithEqualSum.py
+++ 1013-PartitionArrayIntoThreePartsWithEqualSum/1013-PartitionArrayIntoThreePartsWithEqualSum.py
@@ -1,26 +1,17 @@
-# Last updated: 18/04/2025, 14:46:16
+# Last updated: 18/04/2025, 14:48:29
 class Solution(object):
     def canThreePartsEqualSum(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: bool
-        """
-
         total = sum(arr)
         if total % 3 != 0:
             return False
-
         target = total // 3
-        current_sum = count = 0
-
-        for i in range(len(arr) - 1):
+        count, current_sum = 0, 0
+        for i in range(len(arr)):
             current_sum += arr[i]
             if current_sum == target:
-                current_sum = 0
                 count += 1
-                if count == 2:
+                current_sum = 0
+                if count == 2 and i < len(arr) - 1:
                     return True
-        
         return False
 
-        
