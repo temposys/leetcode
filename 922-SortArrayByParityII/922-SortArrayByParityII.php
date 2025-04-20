f4fb09ba9168386d683af300477df6da8063e76b
Yana Tempos <ys.tempos@gmail.com>
2025-04-20 15:21:14 -0700

    Create file

---
+++ 922-SortArrayByParityII/922-SortArrayByParityII.php
@@ -0,0 +1,26 @@
+// Last updated: 20/04/2025, 15:21:13
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function sortArrayByParityII($nums) {
+        $even = 0;
+        $odd = 1;
+
+        while ($even < count($nums) && $odd < count($nums)) {
+            if ($nums[$even] % 2 === 0) {
+                $odd -= 2;
+            } else if ($nums[$odd] % 2 === 1) {
+                $even -= 2;
+            } else {
+                [$nums[$even], $nums[$odd]] = [$nums[$odd], $nums[$even]];
+            }
+            $even +=2;
+            $odd += 2;
+        }
+
+        return $nums;
+    }
+}
