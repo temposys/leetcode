89d7d4fba8f64e4dbd6e8e48b8339f5d6e50d051
Yana Tempos <ys.tempos@gmail.com>
2025-04-23 19:52:05 -0700

    File updated at 23/04/2025, 19:52:05

--- 189-RotateArray/189-RotateArray.php
+++ 189-RotateArray/189-RotateArray.php
@@ -1,17 +1,18 @@
-// Last updated: 23/04/2025, 19:51:59
+// Last updated: 23/04/2025, 19:52:05
 class Solution {
 
-	function rotate(&$nums, $k) {
-        $count = count($nums);
-        if ($count === 0) return $nums;
-
-        $k = $k % $count;
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return NULL
+     */
+    function rotate(&$nums, $k) {
+        $len = count($nums);
+        if ($len === 1) return $nums;
+        
+        $k = $k % $len;
         if ($k === 0) return $nums;
 
-        $nums = array_merge(
-            array_slice($nums, -$k),
-            array_slice($nums, 0, $count - $k),
-        );
-        return $nums;
+        $nums = array_merge(array_slice($nums, -$k), array_slice($nums, 0, $len - $k));
     }
 }
