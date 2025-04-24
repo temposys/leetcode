a09a46c1bd4f6c139c5706985dffc317e72ecccb
Yana Tempos <ys.tempos@gmail.com>
2025-04-24 16:45:20 -0700

    Create file

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,14 @@
+# Last updated: 24/04/2025, 16:45:19
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return True if goal == 0 else False
