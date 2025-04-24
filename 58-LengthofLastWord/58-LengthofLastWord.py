713466892198951df38a7f0836b746b7cd219e99
Yana Tempos <ys.tempos@gmail.com>
2025-04-23 20:07:27 -0700

    Create file

---
+++ 58-LengthofLastWord/58-LengthofLastWord.py
@@ -0,0 +1,8 @@
+# Last updated: 23/04/2025, 20:07:27
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.strip().split(" ")[-1])
