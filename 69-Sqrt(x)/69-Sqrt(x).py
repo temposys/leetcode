c05b8785da33626ea32bbabed0dbae9d0cdd2646
Yana Tempos <ys.tempos@gmail.com>
2025-04-26 14:23:50 -0700

    File updated at 26/04/2025, 14:23:50

--- 69-Sqrt(x)/69-Sqrt(x).py
+++ 69-Sqrt(x)/69-Sqrt(x).py
@@ -1,24 +1,22 @@
-# Last updated: 26/04/2025, 14:19:42
+# Last updated: 26/04/2025, 14:23:50
 class Solution:
     def mySqrt(self, x: int) -> int:
         if x == 0 or x == 1:
             return x
             
-        low = 0
+        low = 2
         high = x // 2
         root = 0
 
         while low <= high:
             mid = low + ((high - low) // 2)
-            square = mid * mid
 
-            if square == x:
+            if mid == x // mid:
                 return mid
             
-            if square < x:
-                root = max(root, mid)
+            if mid < x // mid:
                 low = mid + 1
             else:
                 high = mid - 1
         
-        return root
+        return high
