e4bc8ce43d5aa8a995f3c2fad8dec61dc3d4b2d2
Yana Tempos <ys.tempos@gmail.com>
2025-04-29 15:17:20 -0700

    File updated at 29/04/2025, 15:17:20

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -1,18 +1,42 @@
-# Last updated: 29/04/2025, 14:27:32
+# Last updated: 29/04/2025, 15:17:20
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
-        seen = set()
-        ans = []
-        for i in range(n):
-            for j in range(i+1, n):
-                for k in range(j+1, n):
-                    lastNumber = target - nums[i] - nums[j] - nums[k]
-                    if lastNumber in seen:
-                        arr = sorted([nums[i], nums[j], nums[k], lastNumber])
-                        new_ans = [arr[0], arr[1], arr[2], arr[3]]
-                        if new_ans not in ans:
-                            ans.append(new_ans)
-            seen.add(nums[i])
-        return ans
+        nums.sort()
 
+        def two_sum(start, target):
+            end = len(nums)-1
+            pairs = []
+            while start < end:
+                sm = nums[start] + nums[end]
+                if sm == target:
+                    pairs.append([nums[start], nums[end]])
+                    start, end = start+1, end-1
+                    while start < end and nums[start] == nums[start-1]:
+                        start += 1
+                elif sm > target:
+                    end -= 1
+                else:
+                    start += 1
+            
+            return pairs
+
+        def k_sum(start, k, target):
+            if len(nums) < k or start == len(nums) or nums[start] * k > target or nums[-1] * k < target:
+                return []
+            
+            if k == 2:
+                return two_sum(start, target)
+
+            quads = []
+            for i in range(start, len(nums)):
+                if start != i and nums[i] == nums[i-1]:
+                    continue
+                lists = k_sum(i+1, k-1, target-nums[i])
+                for lst in lists:
+                    lst.insert(0, nums[i])
+
+                for lst in lists:
+                    quads.append(lst)
+            return quads
+
+        return k_sum(0, 4, target)
