02a33f575b1e70be37b3fe8d0ffd3716e68a74e5
Yana Tempos <ys.tempos@gmail.com>
2025-04-30 08:45:48 -0700

    File updated at 30/04/2025, 08:45:48

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,4 +1,4 @@
-# Last updated: 30/04/2025, 08:44:08
+# Last updated: 30/04/2025, 08:45:48
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
