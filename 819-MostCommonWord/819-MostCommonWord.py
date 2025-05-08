c9046a4cfcb6a28d98463c8896c0fac5d7fe5ac7
Yana Tempos <ys.tempos@gmail.com>
2025-05-07 18:33:56 -0700

    File updated at 07/05/2025, 18:33:56

--- 819-MostCommonWord/819-MostCommonWord.py
+++ 819-MostCommonWord/819-MostCommonWord.py
@@ -1,4 +1,4 @@
-# Last updated: 07/05/2025, 18:31:34
+# Last updated: 07/05/2025, 18:33:56
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         pList = re.findall(r'\w+', paragraph.lower())
@@ -6,4 +6,4 @@ class Solution:
         for ban in set(banned):
             pList = [x for x in pList if x != ban]
 
-        return Counter(pList).most_common(1)[0][0]
+        return max(Counter(pList), key=Counter(pList).get)
