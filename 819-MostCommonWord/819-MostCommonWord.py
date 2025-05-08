b83d5793dadc543ff7d7971361bab8ffa782be3f
Yana Tempos <ys.tempos@gmail.com>
2025-05-07 18:31:34 -0700

    File updated at 07/05/2025, 18:31:34

--- 819-MostCommonWord/819-MostCommonWord.py
+++ 819-MostCommonWord/819-MostCommonWord.py
@@ -1,11 +1,9 @@
-# Last updated: 07/05/2025, 18:28:20
+# Last updated: 07/05/2025, 18:31:34
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        paragraph = re.sub(r"[^a-z0-9 ]", ' ', paragraph.lower())
-        paragraph = ' '.join(paragraph.split())
-        pList = paragraph.split(" ")
+        pList = re.findall(r'\w+', paragraph.lower())
 
-        for ban in banned:
+        for ban in set(banned):
             pList = [x for x in pList if x != ban]
 
         return Counter(pList).most_common(1)[0][0]
