91b9bf48a99ded7c3d0328c713166affc871bc97
Yana Tempos <ys.tempos@gmail.com>
2025-05-21 20:41:23 -0700

    File updated at 21/05/2025, 20:41:23

--- 637-AverageofLevelsinBinaryTree/637-AverageofLevelsinBinaryTree.py
+++ 637-AverageofLevelsinBinaryTree/637-AverageofLevelsinBinaryTree.py
@@ -1,30 +1,26 @@
-# Last updated: 21/05/2025, 20:31:50
+# Last updated: 21/05/2025, 20:41:23
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        def bfs(node, level, arr):
-            if node is None:
-                return
-            if len(arr) < level+1:
-                arr.append([node.val])
-            else:
-                arr[level].append(node.val)
-            bfs(node.left, level + 1, arr)
-            bfs(node.right, level + 1, arr)
-            return
-        
-        arr = []
-        bfs(root, 0, arr)
 
-        res = []
-        for i in arr:
-            res.append(round(sum(i)/len(i), 5))
-        
-        return res
-
-        
+        avg = []
+        queue = deque([root])   # There is at least one node
+        while queue:
+            num_nodes_level = len(queue)
+            sum_level = 0
+            for _ in range(num_nodes_level):
+                cur_node = queue.popleft()
+                sum_level += cur_node.val
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                if cur_node.right:
+                    queue.append(cur_node.right)
+            avg.append(sum_level/num_nodes_level)
+        return avg
