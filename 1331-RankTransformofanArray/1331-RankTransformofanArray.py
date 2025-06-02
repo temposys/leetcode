e72f0819b6c7ab2f1c2b89a15c2a44c9fb46a874
Yana Tempos <ys.tempos@gmail.com>
2025-06-01 20:38:01 -0700

    File updated at 01/06/2025, 20:38:01

--- 1331-RankTransformofanArray/1331-RankTransformofanArray.py
+++ 1331-RankTransformofanArray/1331-RankTransformofanArray.py
@@ -1,8 +1,8 @@
-# Last updated: 01/06/2025, 20:37:19
+# Last updated: 01/06/2025, 20:38:01
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         num_to_rank = {}
-        sorted_arr = list(sorted(set(arr)))
+        sorted_arr = sorted(set(arr))
 
         for i in range(len(sorted_arr)):
             num_to_rank[sorted_arr[i]] = i+1
