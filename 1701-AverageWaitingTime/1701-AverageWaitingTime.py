cec7f07a8ba37b6b4ea8bcbc1644596643aa5d15
Yana Tempos <ys.tempos@gmail.com>
2025-09-14 17:32:50 -0700

    File updated at 14/09/2025, 17:32:50

--- 1701-AverageWaitingTime/1701-AverageWaitingTime.py
+++ 1701-AverageWaitingTime/1701-AverageWaitingTime.py
@@ -1,13 +1,10 @@
-# Last updated: 14/09/2025, 17:30:30
+# Last updated: 14/09/2025, 17:32:50
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
         totalWaiting = 0
         endTime = 0
-        for customer in customers:
-            orderTime, timeToCook = customer
-            startTime = endTime if endTime > orderTime else orderTime
-            endTime = startTime + timeToCook
-            waitingTime = endTime - orderTime
-            totalWaiting += waitingTime
+        for orderTime, timeToCook  in customers:
+            endTime = (endTime if endTime > orderTime else orderTime) + timeToCook
+            totalWaiting += endTime - orderTime
 
         return totalWaiting / len(customers)
