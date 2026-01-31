ce6c0fd715f228b12e13da6dab3fcdd59fa96691
Yana Tempos <ys.tempos@gmail.com>
2026-01-31 12:36:29 -0800

    File updated at 31/01/2026, 12:36:28

--- 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.py
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.py
@@ -1,15 +1,16 @@
-# Last updated: 31/01/2026, 12:28:39
-1class Solution:
-2    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-3        letters.append(target)
-4        counter = Counter(letters)
-5        uchars = set(counter.keys())
-6
-7        uchars = sorted(uchars, reverse = False)
-8
-9        if uchars[-1] == target: return letters[0]
-10        
-11        for k,v in enumerate(uchars):
-12            if v == target: return uchars[k+1]
-13
-14        return letters[0]
+# Last updated: 31/01/2026, 12:36:28
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        start = 0
+        end = len(letters)
+
+        while start< end:
+            mid = (start+end)//2
+            if letters[mid] > target:
+                end = mid
+            else:
+                start = mid+1
+        if start>= len(letters):
+            return letters[0]
+        else:
+            return letters[start]
