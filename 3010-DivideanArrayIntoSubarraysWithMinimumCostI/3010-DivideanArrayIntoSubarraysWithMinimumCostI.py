8717c5751b37a031b6a09469907adf5da1c66827
Yana Tempos <ys.tempos@gmail.com>
2026-01-31 19:39:48 -0800

    File updated at 31/01/2026, 19:39:47

--- 3010-DivideanArrayIntoSubarraysWithMinimumCostI/3010-DivideanArrayIntoSubarraysWithMinimumCostI.py
+++ 3010-DivideanArrayIntoSubarraysWithMinimumCostI/3010-DivideanArrayIntoSubarraysWithMinimumCostI.py
@@ -1,10 +1,11 @@
-# Last updated: 31/01/2026, 19:37:07
-1class Solution:
-2    def minimumCost(self, nums: List[int]) -> int:
-3        summ = nums[0]
-4        nums.remove(nums[0])
-5        min1 = min(nums)
-6        nums.remove(min1)
-7        min2 = min(nums)
-8
-9        return summ + min1 + min2
+# Last updated: 31/01/2026, 19:39:47
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+        first = nums[0]
+        rest = nums[1:]
+        
+        rest.sort()
+        
+        return first + rest[0] + rest[1]
+
+        
